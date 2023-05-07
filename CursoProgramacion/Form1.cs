bd6724fa22dbe7ddbfa70f97d6f9dfd097ef14e3
Carlos Perez <per.ckarlos@gmail.com>
2023-05-06 21:30:44 -0500

    iniciando repo con C#

---
+++ CursoProgramacion/Form1.cs
@@ -0,0 +1,15 @@
+namespace CursoProgramacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Texto cambiado";
+        }
+    }
+}
